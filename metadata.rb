42875172bb0bafed52f8ac6f270f399a719ecba7
Eric Richardson <e@ericrichardson.com>
2015-02-10 16:58:07 -0500

    scpr-elasticsearch 0.1.1: Fix service check script

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'erichardson@scpr.org'
 license          'all_rights'
 description      'Installs/Configures scpr-elasticsearch'
 long_description 'Installs/Configures scpr-elasticsearch'
-version          '0.1.0'
+version          '0.1.1'
 
 depends "java"
 depends "elasticsearch"
