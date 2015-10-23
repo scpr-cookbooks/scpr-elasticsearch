006759d0ddcbee2ff21493a1204abc1be75ad1e3
Eric Richardson <e@ericrichardson.com>
2015-10-23 16:46:17 -0400

    0.2.0: Use Elasticsearch 1.7.2

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,18 +4,10 @@ default.scpr_elasticsearch.consul       = true
 
 #----------
 
-include_attribute "java"
-
-default.java.install_flavor = "oracle"
-default.java.jdk_version    = 7
-default.java.oracle.accept_oracle_download_terms = true
-
-#----------
-
 include_attribute "elasticsearch"
 
 override.elasticsearch.zen.ping.multicast.enabled = node.scpr_elasticsearch.discovery
-override.elasticsearch.version        = "1.4.1"
-override.elasticsearch.download_url   = "http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.1.tar.gz"
+override.elasticsearch.version        = "1.7.2"
+override.elasticsearch.download_url   = "http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.2.tar.gz"
 default.elasticsearch.path.data       = "/data/elasticsearch"
 default.elasticsearch.path.logs       = "/var/log/elasticsearch"
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,30 +6,32 @@
 
 node.set.elasticsearch.cluster.name = node.scpr_elasticsearch.cluster_name
 
-include_recipe "java"
+include_recipe "scpr-java"
 include_recipe "elasticsearch"
 
-# -- Set up Consul Service -- #
+if node.scpr_elasticsearch.consul
+  # -- Set up Consul Service -- #
 
-include_recipe "scpr-consul"
+  include_recipe "scpr-consul"
 
-# write our service check script...
-check_script = "#{node.scpr_consul.checks_dir}/elasticsearch-#{node.scpr_elasticsearch.cluster_name}.rb"
-template check_script do
-  action    :create
-  source    "es-service-check.rb.erb"
-  mode      0755
-  variables({
-    cluster_name: node.scpr_elasticsearch.cluster_name
-  })
-end
+  # write our service check script...
+  check_script = "#{node.scpr_consul.checks_dir}/elasticsearch-#{node.scpr_elasticsearch.cluster_name}.rb"
+  template check_script do
+    action    :create
+    source    "es-service-check.rb.erb"
+    mode      0755
+    variables({
+      cluster_name: node.scpr_elasticsearch.cluster_name
+    })
+  end
 
-consul_service_def "es-#{node.scpr_elasticsearch.cluster_name}" do
-  action    :create
-  check({
-    interval: '30s',
-    script:   check_script,
-  })
-  tags      ["elasticsearch"]
-  notifies  :reload, "service[consul]"
+  consul_service_def "es-#{node.scpr_elasticsearch.cluster_name}" do
+    action    :create
+    check({
+      interval: '30s',
+      script:   check_script,
+    })
+    tags      ["elasticsearch"]
+    notifies  :reload, "service[consul]"
+  end
 end
